bde4fe033e42c66cd36c45eda58ec1d865ca76f6
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-09-08 20:23:51 -0400

    Replace Client use of cookies with auth headers

--- src/client.ts
+++ src/client.ts
@@ -4,10 +4,15 @@ export class Client {
   public axios: AxiosInstance;
 
   constructor(baseURL: string) {
-    this.axios = axios.create({
-      baseURL,
-      withCredentials: true,
-    });
+    this.axios = axios.create({ baseURL });
+  }
+
+  public setAuthToken(token: string) {
+    this.axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  }
+
+  public removeAuthToken() {
+    delete this.axios.defaults.headers.common.Authorization;
   }
 
   public get(path: string, params: {} = {}): Promise<any> {
